da6f73be16e414837d988665f730bb38929b4172
Oğuzhan Kandakoğlu <oguzhankandakoglu@gmail.com>
2019-07-09 16:53:27 +0300

    inital

---
+++ LabelTool YoloV3/LabelTool YoloV3/Form1.Designer.cs
@@ -0,0 +1,351 @@
+﻿namespace LabelTool_YoloV3
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnImgIn = new System.Windows.Forms.Button();
+            this.tbImgIn = new System.Windows.Forms.TextBox();
+            this.tbLabelOut = new System.Windows.Forms.TextBox();
+            this.btnLabelOut = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.FolderInfo = new System.Windows.Forms.Label();
+            this.gbFolder = new System.Windows.Forms.GroupBox();
+            this.gbLabeling = new System.Windows.Forms.GroupBox();
+            this.tbLabelList = new System.Windows.Forms.RichTextBox();
+            this.btnAddClass = new System.Windows.Forms.Button();
+            this.cbClasses = new System.Windows.Forms.ComboBox();
+            this.gbNavigating = new System.Windows.Forms.GroupBox();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.btnPrev = new System.Windows.Forms.Button();
+            this.btnImgTo = new System.Windows.Forms.Button();
+            this.tbImgTo = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.gbInfo = new System.Windows.Forms.GroupBox();
+            this.lblPointEnd = new System.Windows.Forms.Label();
+            this.lblPointStart = new System.Windows.Forms.Label();
+            this.lblHeight = new System.Windows.Forms.Label();
+            this.lblWidth = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.gbFolder.SuspendLayout();
+            this.gbLabeling.SuspendLayout();
+            this.gbNavigating.SuspendLayout();
+            this.gbInfo.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(800, 600);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // btnImgIn
+            // 
+            this.btnImgIn.Location = new System.Drawing.Point(6, 32);
+            this.btnImgIn.Name = "btnImgIn";
+            this.btnImgIn.Size = new System.Drawing.Size(75, 23);
+            this.btnImgIn.TabIndex = 1;
+            this.btnImgIn.Text = "Image Input";
+            this.btnImgIn.UseVisualStyleBackColor = true;
+            this.btnImgIn.Click += new System.EventHandler(this.btnImgIn_Click);
+            // 
+            // tbImgIn
+            // 
+            this.tbImgIn.Location = new System.Drawing.Point(87, 32);
+            this.tbImgIn.Name = "tbImgIn";
+            this.tbImgIn.Size = new System.Drawing.Size(329, 20);
+            this.tbImgIn.TabIndex = 2;
+            // 
+            // tbLabelOut
+            // 
+            this.tbLabelOut.Location = new System.Drawing.Point(87, 61);
+            this.tbLabelOut.Name = "tbLabelOut";
+            this.tbLabelOut.Size = new System.Drawing.Size(329, 20);
+            this.tbLabelOut.TabIndex = 4;
+            // 
+            // btnLabelOut
+            // 
+            this.btnLabelOut.Location = new System.Drawing.Point(6, 61);
+            this.btnLabelOut.Name = "btnLabelOut";
+            this.btnLabelOut.Size = new System.Drawing.Size(75, 23);
+            this.btnLabelOut.TabIndex = 3;
+            this.btnLabelOut.Text = "LabelOutput";
+            this.btnLabelOut.UseVisualStyleBackColor = true;
+            this.btnLabelOut.Click += new System.EventHandler(this.btnLabelOut_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(341, 46);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 6;
+            this.btnDelete.Text = "Delete(Last)";
+            this.btnDelete.UseVisualStyleBackColor = true;
+
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(341, 113);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 7;
+            this.btnClear.Text = "ClearAll";
+            this.btnClear.UseVisualStyleBackColor = true;
+            
+            // 
+            // FolderInfo
+            // 
+            this.FolderInfo.AutoSize = true;
+            this.FolderInfo.Location = new System.Drawing.Point(338, 55);
+            this.FolderInfo.Name = "FolderInfo";
+            this.FolderInfo.Size = new System.Drawing.Size(54, 13);
+            this.FolderInfo.TabIndex = 8;
+            this.FolderInfo.Text = "FolderInfo";
+            // 
+            // gbFolder
+            // 
+            this.gbFolder.Controls.Add(this.btnImgIn);
+            this.gbFolder.Controls.Add(this.tbImgIn);
+            this.gbFolder.Controls.Add(this.btnLabelOut);
+            this.gbFolder.Controls.Add(this.tbLabelOut);
+            this.gbFolder.Location = new System.Drawing.Point(806, 12);
+            this.gbFolder.Name = "gbFolder";
+            this.gbFolder.Size = new System.Drawing.Size(422, 100);
+            this.gbFolder.TabIndex = 9;
+            this.gbFolder.TabStop = false;
+            this.gbFolder.Text = "FolderOptions";
+            // 
+            // gbLabeling
+            // 
+            this.gbLabeling.Controls.Add(this.tbLabelList);
+            this.gbLabeling.Controls.Add(this.btnClear);
+            this.gbLabeling.Controls.Add(this.btnAddClass);
+            this.gbLabeling.Controls.Add(this.btnDelete);
+            this.gbLabeling.Controls.Add(this.cbClasses);
+            this.gbLabeling.Location = new System.Drawing.Point(806, 118);
+            this.gbLabeling.Name = "gbLabeling";
+            this.gbLabeling.Size = new System.Drawing.Size(422, 147);
+            this.gbLabeling.TabIndex = 10;
+            this.gbLabeling.TabStop = false;
+            this.gbLabeling.Text = "Labeling";
+            // 
+            // tbLabelList
+            // 
+            this.tbLabelList.Location = new System.Drawing.Point(6, 46);
+            this.tbLabelList.Name = "tbLabelList";
+            this.tbLabelList.Size = new System.Drawing.Size(324, 90);
+            this.tbLabelList.TabIndex = 10;
+            this.tbLabelList.Text = "";
+            this.tbLabelList.WordWrap = false;
+            // 
+            // btnAddClass
+            // 
+            this.btnAddClass.Location = new System.Drawing.Point(341, 17);
+            this.btnAddClass.Name = "btnAddClass";
+            this.btnAddClass.Size = new System.Drawing.Size(75, 23);
+            this.btnAddClass.TabIndex = 9;
+            this.btnAddClass.Text = "Add Class";
+            this.btnAddClass.UseVisualStyleBackColor = true;
+            // 
+            // cbClasses
+            // 
+            this.cbClasses.FormattingEnabled = true;
+            this.cbClasses.Location = new System.Drawing.Point(6, 19);
+            this.cbClasses.Name = "cbClasses";
+            this.cbClasses.Size = new System.Drawing.Size(324, 21);
+            this.cbClasses.TabIndex = 8;
+            // 
+            // gbNavigating
+            // 
+            this.gbNavigating.Controls.Add(this.btnNext);
+            this.gbNavigating.Controls.Add(this.btnPrev);
+            this.gbNavigating.Controls.Add(this.btnImgTo);
+            this.gbNavigating.Controls.Add(this.tbImgTo);
+            this.gbNavigating.Controls.Add(this.FolderInfo);
+            this.gbNavigating.Location = new System.Drawing.Point(806, 271);
+            this.gbNavigating.Name = "gbNavigating";
+            this.gbNavigating.Size = new System.Drawing.Size(422, 83);
+            this.gbNavigating.TabIndex = 10;
+            this.gbNavigating.TabStop = false;
+            this.gbNavigating.Text = "Navigating";
+            // 
+            // btnNext
+            // 
+            this.btnNext.Location = new System.Drawing.Point(87, 19);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(75, 23);
+            this.btnNext.TabIndex = 10;
+            this.btnNext.Text = "Next >>>";
+            this.btnNext.UseVisualStyleBackColor = true;
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
+            // 
+            // btnPrev
+            // 
+            this.btnPrev.Location = new System.Drawing.Point(6, 19);
+            this.btnPrev.Name = "btnPrev";
+            this.btnPrev.Size = new System.Drawing.Size(75, 23);
+            this.btnPrev.TabIndex = 9;
+            this.btnPrev.Text = "<<< Prev";
+            this.btnPrev.UseVisualStyleBackColor = true;
+            this.btnPrev.Click += new System.EventHandler(this.btnPrev_Click);
+            // 
+            // btnImgTo
+            // 
+            this.btnImgTo.Location = new System.Drawing.Point(341, 19);
+            this.btnImgTo.Name = "btnImgTo";
+            this.btnImgTo.Size = new System.Drawing.Size(75, 23);
+            this.btnImgTo.TabIndex = 5;
+            this.btnImgTo.Text = "GoTo: IMG";
+            this.btnImgTo.UseVisualStyleBackColor = true;
+            this.btnImgTo.Click += new System.EventHandler(this.btnImgTo_Click);
+            // 
+            // tbImgTo
+            // 
+            this.tbImgTo.Location = new System.Drawing.Point(260, 22);
+            this.tbImgTo.Name = "tbImgTo";
+            this.tbImgTo.Size = new System.Drawing.Size(75, 20);
+            this.tbImgTo.TabIndex = 6;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 50;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // gbInfo
+            // 
+            this.gbInfo.Controls.Add(this.lblPointEnd);
+            this.gbInfo.Controls.Add(this.lblPointStart);
+            this.gbInfo.Controls.Add(this.lblHeight);
+            this.gbInfo.Controls.Add(this.lblWidth);
+            this.gbInfo.Location = new System.Drawing.Point(806, 360);
+            this.gbInfo.Name = "gbInfo";
+            this.gbInfo.Size = new System.Drawing.Size(422, 97);
+            this.gbInfo.TabIndex = 11;
+            this.gbInfo.TabStop = false;
+            this.gbInfo.Text = "Info";
+            // 
+            // lblPointEnd
+            // 
+            this.lblPointEnd.AutoSize = true;
+            this.lblPointEnd.Location = new System.Drawing.Point(84, 60);
+            this.lblPointEnd.Name = "lblPointEnd";
+            this.lblPointEnd.Size = new System.Drawing.Size(56, 13);
+            this.lblPointEnd.TabIndex = 5;
+            this.lblPointEnd.Text = "Point End:";
+            // 
+            // lblPointStart
+            // 
+            this.lblPointStart.AutoSize = true;
+            this.lblPointStart.Location = new System.Drawing.Point(81, 33);
+            this.lblPointStart.Name = "lblPointStart";
+            this.lblPointStart.Size = new System.Drawing.Size(59, 13);
+            this.lblPointStart.TabIndex = 4;
+            this.lblPointStart.Text = "Point Start:";
+            // 
+            // lblHeight
+            // 
+            this.lblHeight.AutoSize = true;
+            this.lblHeight.Location = new System.Drawing.Point(252, 60);
+            this.lblHeight.Name = "lblHeight";
+            this.lblHeight.Size = new System.Drawing.Size(51, 13);
+            this.lblHeight.TabIndex = 3;
+            this.lblHeight.Text = "HEIGHT:";
+            // 
+            // lblWidth
+            // 
+            this.lblWidth.AutoSize = true;
+            this.lblWidth.Location = new System.Drawing.Point(256, 33);
+            this.lblWidth.Name = "lblWidth";
+            this.lblWidth.Size = new System.Drawing.Size(47, 13);
+            this.lblWidth.TabIndex = 2;
+            this.lblWidth.Text = "WIDTH:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1240, 601);
+            this.Controls.Add(this.gbInfo);
+            this.Controls.Add(this.gbNavigating);
+            this.Controls.Add(this.gbLabeling);
+            this.Controls.Add(this.gbFolder);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "LabelTool";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.gbFolder.ResumeLayout(false);
+            this.gbFolder.PerformLayout();
+            this.gbLabeling.ResumeLayout(false);
+            this.gbNavigating.ResumeLayout(false);
+            this.gbNavigating.PerformLayout();
+            this.gbInfo.ResumeLayout(false);
+            this.gbInfo.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnImgIn;
+        private System.Windows.Forms.TextBox tbImgIn;
+        private System.Windows.Forms.TextBox tbLabelOut;
+        private System.Windows.Forms.Button btnLabelOut;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Label FolderInfo;
+        private System.Windows.Forms.GroupBox gbFolder;
+        private System.Windows.Forms.GroupBox gbLabeling;
+        private System.Windows.Forms.Button btnAddClass;
+        private System.Windows.Forms.ComboBox cbClasses;
+        private System.Windows.Forms.GroupBox gbNavigating;
+        private System.Windows.Forms.Button btnImgTo;
+        private System.Windows.Forms.TextBox tbImgTo;
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.Button btnPrev;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.RichTextBox tbLabelList;
+        private System.Windows.Forms.GroupBox gbInfo;
+        private System.Windows.Forms.Label lblHeight;
+        private System.Windows.Forms.Label lblWidth;
+        private System.Windows.Forms.Label lblPointEnd;
+        private System.Windows.Forms.Label lblPointStart;
+    }
+}
+
---
+++ LabelTool YoloV3/LabelTool YoloV3/Form1.cs
@@ -0,0 +1,260 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LabelTool_YoloV3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Resim kaynağını seçtiren buton.
+        /// </summary>
+        private void btnImgIn_Click(object sender, EventArgs e)
+        {
+            tbImgIn.Text = GetDirectory(); //Resim kaynağını belirler.
+
+            DirectoryInfo di = new DirectoryInfo(tbImgIn.Text); //İlgili konumdaki bilgileri toplar.
+            FileInfo[] fi = di.GetFiles(); //Tüm dosya bilgilerini bir dizide tutar.
+            img.Totalty = fi.Length; //Toplamdaki dosya sayısı.
+            img.Directory = tbImgIn.Text;
+
+            OpenImage(img.SelectedIMG); //Seçilen klasördeki ilk resimi açar.
+        }
+
+        /// <summary>
+        /// Etiket listesinin kayıt edileceği konumu belirleyen buton.
+        /// </summary>
+        private void btnLabelOut_Click(object sender, EventArgs e)
+        {
+            tbLabelOut.Text = GetDirectory(); // Etiketlerin kayıt edileceği yeri belirler.
+        }
+
+        /// <summary>
+        /// FolderBrowserDialog kullanarak kullanıcıya klasör seçimi yaptırır.
+        /// </summary>
+        /// <returns>Seçilen klasörün dosya yolunu döndürür.</returns>
+        private string GetDirectory()
+        {
+            //Dosya gezginini aç.
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            //Masaüstünden başlat.
+            fbd.RootFolder = Environment.SpecialFolder.Desktop;
+            //Sonucu ilgili değişkene kayıt et ve tb'ye yazdır.
+            fbd.ShowDialog();
+            return fbd.SelectedPath;
+        }
+
+        /// <summary>
+        /// Seçilen resim ve toplamdaki resim sayısını belirler. 
+        /// Resim kaynağının dosya yolunu da içinde tutar.
+        /// </summary>
+        private struct IMG
+        {
+            public int SelectedIMG { get; set; } //Seçilen ve işlem yapılmamış resim. 
+            public int Totalty { get; set; } //Seçilen klasörde toplam bulunan resim sayısı.
+            public string Directory { get; set; } //Resim kaynağının dosya yolu.
+            public static int WIDTH = 800; //En
+            public static int HEIGHT = 600; //Boy
+        }
+        private IMG img = new IMG();
+
+        /// <summary>
+        /// 01/10 tarzı seçili dosya/toplam dosya tarzını bastırır.
+        /// </summary>
+        private void NavInfo()
+        {
+            FolderInfo.Text = img.SelectedIMG.ToString() + "/" + img.Totalty.ToString();
+        }
+
+        /// <summary>
+        /// Seçilen indexdeki resmi istenilern çöznürlüğe çevirip PictureBox'a gönderir.
+        /// </summary>
+        /// <param name="_ID">Resmin index'i</param>
+        private void OpenImage(int _ID)
+        {
+            Bitmap bmp = new Bitmap(new Bitmap(GetPath(_ID)), IMG.WIDTH, IMG.HEIGHT);
+            pictureBox1.Image = bmp;
+        }
+
+        /// <summary>
+        /// //İstenilen sıradaki resimin dosya yolunu verir.
+        /// </summary>
+        private string GetPath(int _ID)
+        {
+            DirectoryInfo di = new DirectoryInfo(img.Directory); //İlgili konumdaki bilgileri toplar.
+            FileInfo[] fi = di.GetFiles(); //Tüm dosya bilgilerini bir dizide tutar.
+            return img.Directory + "\\" + fi[_ID];
+        }
+
+        /// <summary>
+        /// Çizim olaylarını kontrol eder.
+        /// </summary>
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            NavInfo();
+            RectInfo(rectangle);
+            DrawRects();
+
+        }
+
+        /// <summary>
+        /// Sonraki indexdeki resimi açar.
+        /// </summary>
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+            img.SelectedIMG++; //Seçilen indexi bir arttır.
+            OpenImage(img.SelectedIMG); //Resmi aç.
+        }
+
+        /// <summary>
+        /// Bir önceki indexdeki resmi açar.
+        /// </summary>
+        private void btnPrev_Click(object sender, EventArgs e)
+        {
+            //Eğer varsa bir önceki resmi aç.
+            if (img.SelectedIMG != 0) img.SelectedIMG--;
+            OpenImage(img.SelectedIMG);
+        }
+
+        /// <summary>
+        /// TextBox'daki resim indexine ulaşır.
+        /// </summary>
+        private void btnImgTo_Click(object sender, EventArgs e)
+        {
+            int _id = Convert.ToInt32(tbImgTo.Text);
+            if (_id <= img.Totalty)
+            {
+                OpenImage(_id);
+                img.SelectedIMG = _id;
+            }
+        }
+
+        #region PictureBOX
+        private Point p1, p2;
+        private bool isMouseDown = false;
+        private List<Rectangle> rectangles = new List<Rectangle>();
+        private Rectangle rectangle;
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            isMouseDown = true;
+            p1 = e.Location;
+        }
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isMouseDown)
+            {
+                p2 = e.Location;
+                Refresh();
+                
+            }
+        }
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            p2 = e.Location;
+            rectangles.Add(rectangle); // Çizilen dörtgeni listeye ekle.
+            yolos.Add(new YOLO());
+            selectedYolo++;
+            PrintRect(rectangle);
+            isMouseDown = false;
+        }
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            if (img.Directory != "")
+            {
+                rectangle = GetRect(); //Çizilen ölçülere göre bir dörtgen hesapla.
+                e.Graphics.DrawRectangle(Pens.Red, rectangle); //Dörtgeni çiz.
+                
+            }
+        }
+        #endregion
+
+        /// <summary>
+        /// Mouse ile belirlenen ölçü ve konuma göre dörtgen hesaplar.
+        /// </summary>
+        private Rectangle GetRect()
+        {
+            Rectangle rect = new Rectangle()
+            {
+                X = Math.Min(p1.X, p2.X),
+                Y = Math.Min(p1.Y, p2.Y),
+                Width = Math.Abs(p1.X - p2.X),
+                Height = Math.Abs(p1.Y - p2.Y)
+            };
+            return rect;
+        }
+
+        /// <summary>
+        /// Dörtgen listesindeki elemanları çizer.
+        /// </summary>
+        private void DrawRects()
+        {
+            Graphics g = pictureBox1.CreateGraphics();
+            foreach (Rectangle rect in rectangles)
+            {
+                g.DrawRectangle(Pens.Blue, rect);
+            }
+            
+        }
+
+        /// <summary>
+        /// İlgili yolo formatının değişkenlerini içinde tutan data struct.
+        /// </summary>
+        private struct YOLO
+        {
+            public int ClassNum { get; set; }
+            public double[] LABEL { get; set; } //ETİKET
+        }
+        List<YOLO> yolos = new List<YOLO>();
+        private int selectedYolo = -1;
+
+        /// <summary>
+        /// Dörtgenin yolo formatında özelliklerini yazdırır.
+        /// </summary>
+        private void PrintRect(Rectangle rect)
+        {
+            YOLO yolo = new YOLO();
+            yolo.LABEL = new double[5];
+            yolo.LABEL[0] = (rect.X + rect.Width) / 2.0;
+            yolo.LABEL[1] = (rect.Y + rect.Height) / 2.0;
+            yolo.LABEL[2] = 1.0 / rect.Width;
+            yolo.LABEL[3] = 1.0 / rect.Height;
+            yolo.LABEL[0] *= yolo.LABEL[2];
+            yolo.LABEL[1] *= yolo.LABEL[3];
+            yolos[selectedYolo] = yolo;
+            string LABEL = yolo.ClassNum + " " + yolo.LABEL[0] + " " + yolo.LABEL[1] + " " + yolo.LABEL[2] + " " + yolo.LABEL[3];
+
+            tbLabelList.Text += LABEL + "\n";
+        }        
+
+        /// <summary>
+        /// Dörtgenin fiziksel özelliklerini yazdırır.
+        /// </summary>
+        private void RectInfo(Rectangle rect)
+        {
+            string topleft = rect.Location.ToString();
+            Point bottomrightcorner = rect.Location;
+            bottomrightcorner.X += rect.Width;
+            bottomrightcorner.Y += rect.Height;
+
+
+            lblPointStart.Text = "Point Start: " + topleft;
+            lblPointEnd.Text = "Point End: " + bottomrightcorner.ToString();
+            lblHeight.Text = "HEIGHT: " + rect.Height.ToString();
+            lblWidth.Text = "WIDTH: " + rect.Width.ToString();
+        }
+
+    }
+}
